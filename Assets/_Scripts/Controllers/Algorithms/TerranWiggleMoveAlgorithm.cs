29d23542a11be06a42d25a1d9a353b1748f14164
Vladimir Minkin <Vladimir.Minkin@massive.co>
2018-11-13 12:59:38 +0100

    Commit to working example

--- Assets/_Scripts/Controllers/Algorithms/TerranWiggleMoveAlgorithm.cs
+++ Assets/_Scripts/Controllers/Algorithms/TerranWiggleMoveAlgorithm.cs
@@ -91,16 +91,6 @@ public class TerranWiggleMoveAlgorithm : MonoBehaviour, IMovable {
         }
     }
 
-    public Bounds OrthographicBounds(this Camera camera)
-    {
-        float screenAspect = (float)Screen.width / (float)Screen.height;
-        float cameraHeight = camera.orthographicSize * 2;
-        Bounds bounds = new Bounds(
-            camera.transform.position,
-            new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
-        return bounds;
-    }
-
     public void changeSpeed(float value)
     {
         _speed = value;
